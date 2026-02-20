a359c7955061d5a662c0fe3704a57d999797b8f2
Santilr126 <santiago.lopezra@upb.edu.co>
2026-02-20 13:35:14 -0500

    Condicionales Dobles Compuestos
    Condicionales Dobles Compuestos

---
+++ 3.CondicionalesDoblesCompuestos/3.CondicionalesDoblesCompuestos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _3.CondicionalesDoblesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje"Persona exenta de impuestos"*/
+            /*string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Ingrese el valor del sueldo");
+            sueldo=Single.Parse(Console.ReadLine());
+
+            if (sueldo>=3000)
+            {
+                //Si la condicion es verdadera
+                Console.WriteLine($"La pesona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Si la condicion es falsa
+                Console.WriteLine("La persona está exenta de impuestos");
+            }*/
+            /*Crear un algoritmo que permita ingresar dos numeros, si el primer numero es mayor al segundo numero informar su suma y diferencia, en caso contrario informar el producto y la division del primer numero respecto al segundo numero*/
+            float Num1 = 0;
+            float Num2 = 0;
+            Console.WriteLine("Ingrese el primer numero");
+            Num1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            Num2 = Single.Parse(Console.ReadLine());
+
+            if (Num1>Num2)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
