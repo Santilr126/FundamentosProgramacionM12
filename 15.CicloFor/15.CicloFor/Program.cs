caadfa6f556ecfa5a0c521c55f5eabf3b9af97e6
Santilr126 <santiago.lopezra@upb.edu.co>
2026-03-25 13:26:04 -0500

    Ciclo For
    Ciclo For

---
+++ 15.CicloFor/15.CicloFor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _15.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int acumulador = 1;
+
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros numeros enteros es: {acumulador}");*/
+
+            /*Realizar un programa que permita obtener la factorial de un numero entero ingresado por el teclado*/
+
+            /*int numFact;
+            int acumulador = 1;
+
+            Console.WriteLine("Ingrese el numero que desea para el factorial");
+            numFact = int.Parse(Console.ReadLine());
+            
+            for (int contador = 1; contador <=numFact; contador++) 
+            {
+                acumulador *= contador;
+            }
+
+            Console.WriteLine($"El factorial de {numFact} es: {acumulador}");*/
+
+            /*Realizar un programa que permite inprimir por pantalla los numeros multiplos de 5, el usuario debe ingresar por el teclado el rango de numeros a evaluar.*/
+
+            /*int inicioRango;
+            int finalRango;
+
+            Console.WriteLine("Ingrese el inicio del rango:");
+            inicioRango = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el final del rango");
+            finalRango=int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Los numeros multiplos de 5 de {inicioRango} a {finalRango} son:");
+
+            for (int contador = inicioRango; contador <= finalRango; contador++)
+            {
+                if (contador % 5 == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+            }*/
+
+            /*En un grupo de 100 alumnos, se desea saber cuantos de los estudiantes son hombres y cuantos son mujeres, ademas se desea saber cuantos son mayores de edad y cuantos no*/
+
+
+        }
+    }
+}
