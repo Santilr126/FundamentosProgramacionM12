ce84aef06f1d66b2e78f22a0797cc122e02ff581
Santilr126 <santiago.lopezra@upb.edu.co>
2026-04-08 12:48:35 -0500

    Parcial Ciclos
    Parcial Ciclos

---
+++ ParcialCiclos/ParcialCiclos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ParcialCiclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numProductos = 0;
+            float valorProducto;
+            float subtotal;
+            float valorTotal = 0;
+
+            Console.WriteLine("Por favor ingrese el valor de 10 productos");
+
+            do
+            {
+                Console.WriteLine("Ingrese el valor de su producto");
+                valorProducto = Single.Parse(Console.ReadLine());
+
+                if (valorProducto > 10000)
+                {
+                    subtotal = valorProducto + (valorProducto * 0.15f);
+                    valorTotal += subtotal;
+                }
+                else if (valorProducto <= 5000)
+                {
+                    subtotal = valorProducto + (valorProducto * 0.19f);
+                    valorTotal += subtotal;
+                }
+                else
+                {
+                    subtotal = valorProducto;
+                    valorTotal += subtotal;
+                }
+
+                numProductos++;
+                
+            } while (numProductos <= 10);
+
+            Console.Clear();
+            Console.WriteLine($"El valor total de la venta es: {valorTotal}");
+        }
+    }
+}
